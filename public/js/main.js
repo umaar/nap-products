a3aa8cae21f94892014cabe92d78190b382a8fe4
Umar <umar.hansa@gmail.com>
2016-06-25 14:18:24 +0100

    WIP autocomplete

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,30 @@
 function start() {
 	console.log('ready');
 
+	const productCache = new Map();
+
 	function addIndividualProductToList(id) {
-		const liveSearchClass = 'product-list-item-live-search';
-		$('.' + liveSearchClass).remove();
 		const template = Handlebars.templates['productItemList.hbs'];
-		$.getJSON('/api/product/' + id, res => {
-			const html = $(template(res)).addClass(liveSearchClass);
-			$('.product-list').prepend(html);
-		});
+		const liveSearchClass = 'product-list-item-live-search';
+
+		function renderListItem(data) {
+			const html = $(template(data)).addClass(liveSearchClass);
+			const existingLiveSearchProduct = $('.' + liveSearchClass);
+			if (existingLiveSearchProduct.length) {
+				existingLiveSearchProduct.replaceWith(html)
+			} else {
+				$('.product-list').prepend(html);
+			}
+		}
+
+		if (productCache.has(id)) {
+			renderListItem(productCache.get(id));
+		} else {
+			$.getJSON('/api/product/' + id, res => {
+				productCache.set(id, res);
+				renderListItem(res);
+			});
+		}
 	}
 
 	$.getJSON('http://localhost:3000/api/all-product-names', response => {
